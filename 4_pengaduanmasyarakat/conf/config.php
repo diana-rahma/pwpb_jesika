385bf47abbd9a0acff10c3109e36fc134f4ab387
jesikanovanda01 <jesikanovanda00@gmail.com>
2023-02-07 14:51:31 +0700

    9

---
+++ 4_pengaduanmasyarakat/conf/config.php
@@ -0,0 +1,11 @@
+<?php 
+$koneksi = mysqli_connect('localhost','root','','peduli diri');
+
+// if(!$koneksi){
+//     die("Koneksi gagal:".mysqli_connect_error());
+// }
+// else{
+//     echo "Koneksi berhasil";
+// }
+
+?>
