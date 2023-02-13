70f52c911c3a615db45916c02d072d81b16b1031
jesikanovanda01 <jesikanovanda00@gmail.com>
2023-02-13 15:00:17 +0700

    13

--- 5_admin/app/edit/edit_data.php
+++ 5_admin/app/edit/edit_data.php
@@ -11,7 +11,7 @@ $view = mysqli_fetch_array($query);
             </div>
             <!-- /.card-header -->
             <div class="card-body">
-                <form method="get" action="update/update_data.php">
+                <form method="post" action="update/update_data.php" enctype="multipart/form-data">
                     <div class="row">
                         <div class="col-sm-6">
                             <!-- text input -->
@@ -41,6 +41,10 @@ $view = mysqli_fetch_array($query);
                                     <option value="4">4</option>
                                 </select>
                             </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <label class="form-label" for="customFile">Upload Foto</label>
+                            <input type="file" name="foto" class="form-control" id="customFile" />
                         </div>
                     </div>
                     <button  type="submit" class="btn btn-sm btn-info">Simpan</button>
--- 5_admin/app/update/update_data.php
+++ 5_admin/app/update/update_data.php
@@ -1,9 +1,16 @@
 <?php
 include('../../conf/config.php');
-$id = $_GET['id'];
-$nama = $_GET['nama'];
-$nim = $_GET['nim'];
-$smt = $_GET['semester'];
-$query = mysqli_query($koneksi,"UPDATE tb_mahasiswa SET nama='$nama',nim='$nim',semester='$smt' WHERE id='$id'");
+$id = $_POST['id'];
+$nama = $_POST['nama'];
+$nim = $_POST['nim'];
+$smt = $_POST['semester'];
+//nama foto//
+$nama_file = $_FILES['foto']['name'];
+// echo $nama_file;
+
+//lokasi foto//
+$file_tmp = $_FILES['foto']['tmp_name'];
+move_uploaded_file($file_tmp,'../foto/'.$nama_file);
+$query = mysqli_query($koneksi,"UPDATE tb_mahasiswa SET nama='$nama',nim='$nim',semester='$smt',foto='$nama_file' WHERE id='$id'");
 header('location:../index.php?page=data-mahasiswa');
 ?>
